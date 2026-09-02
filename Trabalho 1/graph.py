326083d8a216f9d3b718c8dfc41300e403a33910
Matheus Silva <matheuswnscf@gmail.com>
2026-09-02 14:03:17 -0300

    Adicionando implementação para Lista de Adjacencias e a impressao(print)  das duas representacoes

--- Trabalho 1/graph.py
+++ Trabalho 1/graph.py
@@ -1,5 +1,40 @@
 from pathlib import Path
+from node import Node
 
+def create_empty_list_graph(n):
+    return [Node(None, None) for _ in range(n)]
+
+def create_list_graph(archive_directory):
+    with open(f"{archive_directory}", "r") as file:
+        line = file.readline()
+        n = int(line)
+        graph = create_empty_list_graph(n)
+        last_nodes = [None for _ in range(n)]
+
+        for i in range(n):
+            line = file.readline()
+            line = line.strip().split()
+
+            a = int(line[0]) - 1
+            b = int(line[1]) - 1
+            
+            node_a = Node(b, None)
+            node_b = Node(a, None)
+            
+            if last_nodes[a] == None:
+                graph[a].next = node_a
+            else:
+                last_nodes[a].next = node_a
+
+            if last_nodes[b] == None:
+                graph[b].next = node_b
+            else:
+                last_nodes[b].next = node_b
+
+            last_nodes[a] = last_nodes[a].next if last_nodes[a] != None else graph[a].next
+            last_nodes[b] = last_nodes[b].next if last_nodes[b] != None else graph[b].next
+
+        return graph
 
 def create_empty_matrix_graph(n):
     return [[0 for _ in range(n)] for _ in range(n)]
@@ -23,16 +58,48 @@ def create_matrix_graph(archive_directory):
 
         return graph
 
+def str_adj_list(graph):
+    result = ""
+    for i in range(len(graph)):
+        result += f"{i + 1}: "
+        current_node = graph[i].next
+        while current_node != None:
+            result += f"{current_node.value + 1} -> "
+            current_node = current_node.next
+        result += "None\n"
+    return result
+
+def str_matrix(graph):
+    result = ""
+    for i in range(len(graph)):
+        result += str(graph[i]) + "\n"
+    return result
+
 class Graph():
     def __init__(self, archive,adj_list : bool = False):
         
+        self.adj_list = adj_list
+        
         if adj_list:
-            pass
+            self.graph = create_list_graph(archive)
+            return
         
         self.graph = create_matrix_graph(archive)
+        return
     
+    def __str__(self):
+        
+        if self.adj_list:
+            return str_adj_list(self.graph)
+        
+        return str_matrix(self.graph)
     
 if __name__ == "__main__":
     graph_file = Path(__file__).resolve().parents[2] / "graph1.txt"
-    graph = Graph(graph_file, adj_list=False)
-    print(graph.graph)
+    graph1 = Graph(graph_file, adj_list=False)
+    print(graph1)
+    
+    graph2 = Graph(graph_file, adj_list=True)
+    print(graph2)
+    
+    
